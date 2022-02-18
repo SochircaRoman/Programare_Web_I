b2ceb44e80f5775175776a6a7989ca1aa18244e6
User01 <romansochirca30@gmail.com>
2022-02-18 13:42:10 +0200

    Add Ex01

---
+++ 1-02.02.2020/Ex01/script.js
@@ -0,0 +1,32 @@
+// Task 1
+n = 6;
+
+switch (n) {
+    case 0:
+        alert("Zero");
+        break;
+    case 1:
+        alert("Unu");
+    case 2:
+        alert("Doi");
+        break;
+    case 3:
+        alert("Trei");
+    case 4:
+        alert("Patru");
+        break;
+    case 5:
+        alert("Cinci");
+    case 6:
+        alert("Sase");
+        break;
+    case 7:
+        alert("Sapte");
+    case 8:
+        alert("Opt");
+        break;
+    case 9:
+        alert("Noua");
+    default:
+        alert("Nu ati introdus un numar 0-8!");
+}
