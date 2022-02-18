2375970bf08c1e2482759244d51afea1b5f9484e
User01 <romansochirca30@gmail.com>
2022-02-18 22:11:27 +0200

    Add Ex07

---
+++ 1-02.02.2020/Ex07/script.js
@@ -0,0 +1,16 @@
+// Task 7
+let age = 91;
+
+// V1
+if (!(age >= 14 && age <= 90)) {
+    alert("No");
+} else {
+    alert("Yes");
+}
+
+// V2
+if (age < 14 || age > 90) {
+    alert("No");
+} else {
+    alert("Yes");
+}
