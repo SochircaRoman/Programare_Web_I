c09f157a92c57e5d9dbb747b27f69885cbdb7561
User01 <romansochirca30@gmail.com>
2022-02-18 23:13:13 +0200

    Add Ex09

---
+++ 1-02.02.2020/Ex09/script.js
@@ -0,0 +1,9 @@
+// Task 9
+let userName = prompt("Please enter your name: ");
+alert(`Hi ${userName}!`);
+
+if (confirm(`${userName} do you know JavaScript?`)) {
+    alert("Not bad!");
+} else {
+    alert("You can start now to learn JS!");
+}
