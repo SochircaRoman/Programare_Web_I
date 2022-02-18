08d4fd9212550965b65b82012c3a685fc78d3102
User01 <romansochirca30@gmail.com>
2022-02-18 21:51:42 +0200

    Add Ex06

---
+++ 1-02.02.2020/Ex06/script.js
@@ -0,0 +1,12 @@
+// Task 6
+let num = prompt("Please enter a number ");
+
+if (num > 0) {
+    alert(1);
+} else if (num < 0) {
+    alert(-1);
+} else if (num == 0) {
+    alert(0);
+} else {
+    alert("Error");
+}
