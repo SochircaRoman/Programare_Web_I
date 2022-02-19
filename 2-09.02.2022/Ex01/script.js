02adedafcee6751365e76dacbf317d189765814f
User01 <romansochirca30@gmail.com>
2022-02-19 21:10:45 +0200

    Ad Ex01

---
+++ 2-09.02.2022/Ex01/script.js
@@ -0,0 +1,34 @@
+// Task 1
+const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// Method-1
+for (let i = 0; i <= arr.length - 1; i++) {
+    console.log(arr[i]);
+}
+
+/*
+// Method-2
+let index = 0;
+while (index <= arr.length - 1) {
+    console.log(arr[index]);
+    index++;
+}
+
+// Method-3
+let index = 0;
+do {
+    console.log(arr[index]);
+    index++;
+} while (index <= arr.length - 1);
+
+// Method-4
+for (variable in arr) {
+    console.log(Number(variable));
+}
+
+// Method-5
+arr.forEach(element => console.log(element));
+
+// Method-6
+arr.map(element => console.log(element));
+*/
