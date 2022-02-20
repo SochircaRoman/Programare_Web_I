bc4e2a272fd48fc3589d52e24d1a789418e63bb3
User01 <romansochirca30@gmail.com>
2022-02-20 23:48:19 +0200

    Add Ex10

---
+++ 2-09.02.2022/Ex10/script.js
@@ -0,0 +1,12 @@
+// Task 10
+let array = [8, 7, 2, 2, 3, 4, 2, 5, 5, 6, 7, 8, 5, 9, 0, 1];
+
+function uniqueSorted(arr) {
+    let x = [...new Set(arr)];
+    x.sort();
+    array = x;
+    return array;
+}
+
+console.log(uniqueSorted(array));
+console.log(array);
