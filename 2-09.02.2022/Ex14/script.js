a5264ee37d558963d41d2810a5b276142b7636be
User01 <romansochirca30@gmail.com>
2022-02-23 08:43:41 +0200

    Add Ex14

---
+++ 2-09.02.2022/Ex14/script.js
@@ -0,0 +1,16 @@
+// Task 13
+let user1 = {
+    name: "User 1",
+    age: 123
+}
+
+let user2 = {
+    name: "User 1",
+    age: 123
+}
+
+user2.name = "User 2";
+user2.age = 321;
+
+console.log(user1);
+console.log(user2);
