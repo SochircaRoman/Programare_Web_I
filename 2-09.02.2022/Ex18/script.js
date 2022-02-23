2b71a5a6b605ae557ec7b141ff01cfee2045eb46
User01 <romansochirca30@gmail.com>
2022-02-23 10:49:29 +0200

    Add Ex18

---
+++ 2-09.02.2022/Ex18/script.js
@@ -0,0 +1,21 @@
+// Task 18
+const weekDays = {
+    "Mo": "Понедельник",
+    "Tu": "Среда",
+    "We": "Вторник",
+    "Th": "Четверк",
+    "Fr": "Пятница",
+    "Sa": "Суббота",
+    "Su": "Воскресенье",
+};
+
+const arr = ["Luni", "Marti", "Miercuri", "Joi", "Vineri", "Sambata", "Duminica"];
+function translate(obj) {
+    const keys = Object.keys(weekDays);
+    for (let i = 0; i <= arr.length - 1; i++) {
+        weekDays[keys[i]] = arr[i];
+    }
+}
+
+translate(weekDays);
+console.log(weekDays);
